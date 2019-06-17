ad98c23e97388f0cb5d684324b44b6ff9cce3100
omf2333 <1923208364@qq.com>
2019-06-17 18:59:56 +0800

    fix backgroud pic position error on gitpage

--- src/loading/index.js
+++ src/loading/index.js
@@ -4,7 +4,7 @@ const loading = () => {
     console.log('x.load.begin')
     document.getElementById('light').style.display = 'block'
     document.getElementById('fade').style.display = 'block'
-    document.getElementById('fade').style.background = "#000 url('./css/background.png') no-repeat right top"
+    document.getElementById('fade').style.background = "#000 url('./css/background.png') no-repeat center center"
     let black = document.getElementById('fade')
     black.innerHTML +="<div id='loading-wrapper'><div id='loaderSkeleton' ><div id='loader'></div></div></div>"
     setTimeout(() => hide(),5000)
