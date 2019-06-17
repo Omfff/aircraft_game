4b126ff89897d560f48b2f9a18827b3769950137
omf2333 <1923208364@qq.com>
2019-06-17 11:56:13 +0800

    Updates

---
+++ precache-manifest.8c3288d2bc3bd7284f85554a730ed326.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fce21e08919d5f20db26f9bdeff24ba",
+    "url": "/aircraft_game/16a4cfa169318b5b5303.worker.js"
+  },
+  {
+    "revision": "7bf4398fd25a4e52b41d6900c1164c9a",
+    "url": "/aircraft_game/index.html"
+  },
+  {
+    "revision": "bd9660b3441b4561828a",
+    "url": "/aircraft_game/static/css/main.46c9ba62.chunk.css"
+  },
+  {
+    "revision": "a19d9d94d0e0f14a191f",
+    "url": "/aircraft_game/static/js/2.47668277.chunk.js"
+  },
+  {
+    "revision": "bd9660b3441b4561828a",
+    "url": "/aircraft_game/static/js/main.4c1107ea.chunk.js"
+  },
+  {
+    "revision": "f70b17e57bdda269f2b5",
+    "url": "/aircraft_game/static/js/runtime~main.0815819e.js"
+  },
+  {
+    "revision": "d28fc70f29b3c25bf99f3b63983cffa7",
+    "url": "/aircraft_game/static/media/Explosion_002_Tile_8x8_256x256.d28fc70f.png"
+  },
+  {
+    "revision": "78621f638fada65d5ab24e8b4f89eb3b",
+    "url": "/aircraft_game/static/media/GrassGreenTexture0003.78621f63.jpg"
+  },
+  {
+    "revision": "002f20c3fa9d6ea9cd496da92c104461",
+    "url": "/aircraft_game/static/media/GrassGreenTexture0003_normal.002f20c3.jpg"
+  },
+  {
+    "revision": "da494a885ce544445cd09fbbde67058e",
+    "url": "/aircraft_game/static/media/Rock_08_UV_H_CM_1.da494a88.jpg"
+  },
+  {
+    "revision": "6085c2f7e2c6f24d53852f009cdfdbfe",
+    "url": "/aircraft_game/static/media/Rock_08_UV_H_CM_1_normal.6085c2f7.jpg"
+  },
+  {
+    "revision": "4c8e91a4d7aa09069938dcc87e7c7e2b",
+    "url": "/aircraft_game/static/media/Snow2.4c8e91a4.jpg"
+  },
+  {
+    "revision": "a33d50da063b016852d1d139cf6e73b1",
+    "url": "/aircraft_game/static/media/Water_1_M_Normal.a33d50da.jpg"
+  },
+  {
+    "revision": "639428cf065384aae22d01b529011992",
+    "url": "/aircraft_game/static/media/Water_2_M_Normal.639428cf.jpg"
+  },
+  {
+    "revision": "2f1ab236426c1d5cf582d29688d06129",
+    "url": "/aircraft_game/static/media/explosion1.2f1ab236.ogg"
+  },
+  {
+    "revision": "752472e3de62bb7224e84d7b7ed5d119",
+    "url": "/aircraft_game/static/media/explosion2.752472e3.ogg"
+  },
+  {
+    "revision": "b8ff47f4e17fb1e2d7fb36a04e29f76e",
+    "url": "/aircraft_game/static/media/gatling.b8ff47f4.ogg"
+  },
+  {
+    "revision": "db06d2c4c1f1985f0fe212585967e2a9",
+    "url": "/aircraft_game/static/media/impact1.db06d2c4.ogg"
+  },
+  {
+    "revision": "7d720129509d1a0ac532450bced731b0",
+    "url": "/aircraft_game/static/media/impact2.7d720129.ogg"
+  },
+  {
+    "revision": "7a5b4e82670c04e6395146cdcb07c602",
+    "url": "/aircraft_game/static/media/impact3.7a5b4e82.ogg"
+  },
+  {
+    "revision": "43348926f7eb827d3651c3412707b2f0",
+    "url": "/aircraft_game/static/media/impact4.43348926.ogg"
+  },
+  {
+    "revision": "f0faf674f89b6f143519e8529ea644dd",
+    "url": "/aircraft_game/static/media/missile.f0faf674.ogg"
+  },
+  {
+    "revision": "4939a8d27601adf3dd652e5703b6f679",
+    "url": "/aircraft_game/static/media/smokeparticle.4939a8d2.png"
+  },
+  {
+    "revision": "8cb6275163547b97a9416161e38044e0",
+    "url": "/aircraft_game/static/media/snow_scuffed_ground_1.8cb62751.jpg"
+  },
+  {
+    "revision": "030cb7e826794a60fb6ae417b8fb3a4b",
+    "url": "/aircraft_game/static/media/win.030cb7e8.ogg"
+  }
+]);
